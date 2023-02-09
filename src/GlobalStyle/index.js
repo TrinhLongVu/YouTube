aeacedf4c0b9ba24469eae7c2fdd1db9423732c2
TRINH VU <vucongleo@gmail.com>
2023-02-09 21:46:44 +0700

    project initialization

---
+++ src/GlobalStyle/index.js
@@ -0,0 +1,7 @@
+import './GlobalStyle.scss'
+
+function GlobalStyle({children}) {
+    return ( children );
+}
+
+export default GlobalStyle;
---
+++ src/Pages/Video/index.js
@@ -0,0 +1,5 @@
+function PageVideo() {
+    return ( <h1>Page Video</h1> );
+}
+
+export default PageVideo;
